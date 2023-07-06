75c2759c695c50e0043c0189a8e166b04032e492
L A Minora <leonardo.minora@gmail.com>
2023-07-06 10:35:37 -0300

    adicionado UI de usuarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,77 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+const api = axios.create({
+  baseURL: "https://dummyjson.com/",
+});
+
+const Usuario = (props: any) => {
+  return (
+    <div className='card'>
+      <p>id: {props.usuario.id}</p>
+      <p>Nome: {props.usuario.firstName} {props.usuario.lastName}</p>
+      <p>email: {props.usuario.email}</p>
+      <p>apelido: {props.usuario.username}</p>
+      <p>senha: {props.usuario.password}</p>
+    </div>
+  );
+}
+
+const AppUsuarios = () => {
+  const [usuarios, setUsuarios] = useState([]);
+  const carregarUsuarios = () => {
+    console.log("carregando");
+    api.get("/users?limit=10")
+      .then(
+        (resposta) => {
+          console.log(resposta.data);
+          const lista = resposta.data.users;
+          console.log(lista);
+          setUsuarios(lista);
+      });
+      // usuarios = api.get("/users")
+  }
+
+  // // id, firstName, lastName
+  // // email, username, password
+  // const usuario = {
+  //   id: "1",
+  //   firstName: "",
+  //   lastName: "",
+  //   email: "",
+  //   username: "",
+  //   password: "",
+  // }
+  return (
+    <>
+      <button onClick={carregarUsuarios}>carregar usuários</button>
+      {usuarios.map(
+        (usuario: any) => 
+          <Usuario usuario={usuario} key={usuario.id} />)
+      }
+    </>
+  );
+}
+/*
+    {
+      (usuario.id != "") && Usuario
+    }
+if (usuario.id != "") {
+  return <Usuario />;
+else {
+  return null;
+}
+}
+*/
+
+const App = () => {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppUsuarios />
     </>
-  )
+  );
 }
 
 export default App
